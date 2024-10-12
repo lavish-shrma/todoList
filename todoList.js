0f49df9e011ab94b35c845416b78c99665b6f6b9
lavish-shrma <lavishkumar3435@gmail.com>
2024-10-13 01:28:33 +0530

    changing code for optimization

--- todoList.js
+++ todoList.js
@@ -12,19 +12,18 @@ function renderTodoList(){
 
     let todoListHTML = '';
 
-    for(let i=0; i<todoList.length; i++){
-        const todoObject = todoList[i];
-        const { name, dueDate} = todoObject;
+    todoList.forEach( (todoObject, index) => {
+        const {name, dueDate} = todoObject;
         const html = `
         <div>${name}</div>
         <div>${dueDate}</div>
         <button onclick="
-            todoList.splice(${i},1);
+            todoList.splice(${index},1);
             renderTodoList();
             " class="delete-todo-button">delete</button> 
         `;
         todoListHTML += html;
-    }
+    });
             
     document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
